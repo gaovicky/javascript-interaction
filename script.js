03d1863c1eabb6ff3c4d3afdb5551371e8a5f1a1
gaovicky <vg1234@nyu.edu>
2021-04-08 00:12:55 -0400

    JS interation

---
+++ script.js
@@ -0,0 +1,65 @@
+//declaring the variables that will be used
+var heading;
+var fortuneButton;
+var fortuneInput;
+var fortuneOutput;
+
+//random fortunes
+var fortuneList = [
+"a beautiful, smart, and loving person will be coming into your life",
+"a golden egg of opportunity falls into your lap this month",
+"a good time to finish up old tasks",
+"a lifetime of happiness lies ahead of you",
+"a pleasant surprise is waiting for you",
+"a smile is your personal welcome mat",
+"all will go well with your new project",
+"be careful or you could fall for some tricks today",
+"carve your name on your heart and not on marble",
+"do not let ambitions overshadow small success",
+"everyday in your life is a special occasion",
+];
+
+//choose a random fortune in fortuneList
+var randomIndex = Math.floor( Math.random() * fortuneList.length);
+
+//things that will be executed once the page is ready
+window.addEventListener('DOMContentLoaded', function () {
+
+    heading = document.getElementById("title");
+    fortuneButton = document.getElementById("fortune-button");
+    fortuneInput = document.getElementById("fortune-input");
+    fortuneOutput = document.getElementById("fortune-output");
+
+    fortuneButton.addEventListener("click", demonstrationFunction);
+
+});
+
+function demonstrationFunction () {
+
+    console.log("Called function...");
+
+    //retreive the input (name)
+    var currentInputText = fortuneInput.value;
+    console.log("Name is: " + currentInputText);
+
+    fortuneOutput.innerText = currentInputText + ", your fortune cookie says: "+ fortuneList[randomIndex];
+
+    //creating a random color every time
+    var randomRed = Math.random() * 225;
+    var randomGreen = Math.random() * 225;
+    var randomBlue = Math.random() * 225;
+
+    var outputColorString = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+    heading.style.color = outputColorString;
+    fortuneOutput.style.color = outputColorString;
+
+    //make the heading bold and oblique every other time you press the button
+    console.log("Class list BEFORE:");
+    console.log(heading.classList);
+
+    heading.classList.toggle("specialTitle");
+
+    console.log("Class list AFTER:");
+    console.log(heading.classList);
+
+};
